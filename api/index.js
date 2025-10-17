01b4512643ab795a77641f1ee2dd8deb8ae124b0
Ahmed Zubair Memon <ahmedzubairmemon21@gmail.com>
2025-10-18 00:35:54 +0500

    hackathon

--- api/index.js
+++ api/index.js
@@ -10,7 +10,10 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors());
+app.use(cors({
+  origin: "https://hackathon-7qp7.vercel.app/",
+  credentials: true
+}));
 
 app.use(express.json())
 app.use(cookieParser())
