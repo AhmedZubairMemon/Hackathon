c930d71a7061bedf7438558c28977c1a99a941c0
Ahmed Zubair Memon <ahmedzubairmemon21@gmail.com>
2025-10-19 07:45:37 +0500

    hackathon

--- client/src/pages/UploadReport.jsx
+++ client/src/pages/UploadReport.jsx
@@ -20,7 +20,7 @@ const UploadReport = () => {
     setMessage("");
 
     try {
-      const res = await axios.post("http://localhost:8000/api/upload/upload", formData, {
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}api/upload/upload`, formData, {
         headers: { "Content-Type": "multipart/form-data" },
       });
       setImageUrl(res.data.url);
